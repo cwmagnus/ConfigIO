0dd299251968f688e921c10542a5be603f9ad4ff
Christopher Magnuson <cwmagnus0@gmail.com>
2019-05-17 21:57:20 -0400

    Added blank ConfigFile class
    Added starter code for the ConfigFile class so the cmake build system could be tested. This class will be filled out with logic that will handle reading and writing config files.

---
+++ src/ConfigFile.cpp
@@ -0,0 +1,14 @@
+#include "ConfigFile.h"
+
+namespace ConfigIO
+{
+    ConfigFile::ConfigFile()
+    {
+
+    }
+
+    ConfigFile::~ConfigFile()
+    {
+
+    }
+} // namespace ConfigIO
---
+++ src/ConfigFile.h
@@ -0,0 +1,16 @@
+#ifndef CONFIG_IO_CONFIG_FILE_H
+#define CONFIG_IO_CONFIG_FILE_H
+
+#include <string>
+
+namespace ConfigIO
+{
+    class ConfigFile
+    {
+    public:
+        ConfigFile();
+        ~ConfigFile();
+    };
+} // namespace ConfigIO
+
+#endif // !CONFIG_IO_CONFIG_FILE_H
